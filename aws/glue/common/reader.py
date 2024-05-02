04e10b6898d27b77f57f579cc49e87d3b616d361
Jihed-Selmi <jihed.selmi@talan.com>
2024-05-02 15:40:29 +0100

    Added processing in aws glue code

--- aws/glue/common/reader.py
+++ aws/glue/common/reader.py
@@ -4,8 +4,8 @@ from datetime import datetime as dt
 
 from pyspark.sql import DataFrame
 from pyspark.sql.types import StructType
-from .s3 import list_objects_names
-from ..context.context import spark
+from glue.common.s3 import list_objects_names
+from glue.context.context import spark
 
 
 def read_from_parquet(parquet_file_path: str) -> DataFrame:
